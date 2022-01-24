15da25ad4b56dea09ef5ef64c5d3926a91ceedf7
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-01-25 02:28:32 +0900

    로그인 유지 기능 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import GlobalStyle from '../styles/GlobalStyle';
 import { wrapper } from '../store';
 import { cookieStringToObject } from '../lib/utils';
 import axios, { CommonHeaderProperties } from '../lib/api';
+import { meAPI } from '../lib/api/auth';
+import { userActions } from '../store/user';
 
 const app = ({ Component, pageProps }: AppProps) => {
     return (
@@ -14,14 +16,27 @@ const app = ({ Component, pageProps }: AppProps) => {
     );
 };
 
-app.getInitialProps = async (context: AppContext) => {
-    const appInitialProps = await App.getInitialProps(context);
-    const cookieObject = cookieStringToObject(context.ctx.req?.headers.cookie);
-    axios.defaults.headers = {
-        cookie: cookieObject.access_token,
-    } as CommonHeaderProperties;
-    console.log(cookieObject);
-    return { ...appInitialProps };
-};
+app.getInitialProps = wrapper.getInitialAppProps(
+    (store) => async (context: AppContext) => {
+        const appInitialProps = await App.getInitialProps(context);
+        const cookieObject = cookieStringToObject(
+            context.ctx.req?.headers.cookie
+        );
+        const { isLogged } = store.getState().user;
+        try {
+            // 리덕스 스토어의 isLogged가 true이거나 access_token이 없다면 api 보내지 않음
+            if (!isLogged && cookieObject.access_token) {
+                axios.defaults.headers = {
+                    cookie: cookieObject.access_token,
+                } as CommonHeaderProperties;
+                const { data } = await meAPI();
+                store.dispatch(userActions.setLoggedUser(data));
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        return { ...appInitialProps };
+    }
+);
 
 export default wrapper.withRedux(app);
