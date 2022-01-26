a1eb356845a366e75b9d4354241cbc84aadd0d57
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-01-26 20:30:38 +0900

    공통 Selector 컴포넌트 타입구분 추가

--- components/common/Selector.tsx
+++ components/common/Selector.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { useSelector } from '../../store';
 import palette from '../../styles/palette';
+import WarningIcon from '../icons/WarningIcon';
 
-const Container = styled.div<{ isValid: boolean; validateMode: boolean }>`
+// const Container = styled.div<{ isValid: boolean; validateMode: boolean }>`
+const normalSelectorStyle = css`
     width: 100%;
     height: 46px;
 
@@ -25,47 +27,113 @@ const Container = styled.div<{ isValid: boolean; validateMode: boolean }>`
             border-color: ${palette.dark_cyan};
         }
     }
+`;
 
-    ${({ isValid, validateMode }) =>
-        validateMode &&
-        css`
-            select {
-                border-color: ${isValid
-                    ? palette.dark_cyan
-                    : palette.tawny} !important;
-                background-color: ${isValid ? 'white' : palette.snow};
+const RegisterSelectorStyle = css`
+    width: 100%;
+    label {
+        position: relative;
+    }
+    span {
+        display: block;
+        font-size: 16px;
+        color: ${palette.gray_76};
+        font-weight: 600;
+        margin-bottom: 8px;
+    }
+    select {
+        width: 100%;
+        height: 56px;
+        border-radius: 8px;
+        border: 1px solid ${palette.gray_b0};
+        padding: 0 14px 0 12px;
+        appearance: none;
+        outline: none;
+        -webkit-appearance: none;
+        background-image: url('/static/svg/common/selector/register_selector_down_arrow.svg');
+        background-position: right 14px center;
+        background-repeat: no-repeat;
+    }
+`;
+
+interface SelectorContainerProps {
+    isValid: boolean;
+    validateMode: boolean;
+    type: 'register' | 'normal';
+}
+
+const Container = styled.div<SelectorContainerProps>`
+    ${({ type }) => type === 'normal' && normalSelectorStyle};
+    ${({ type }) => type === 'register' && RegisterSelectorStyle};
+    select {
+        ${({ isValid, validateMode }) => {
+            if (validateMode) {
+                if (!isValid) {
+                    return css`
+                        border-color: ${palette.tawny};
+                        background-color: ${palette.snow};
+                    `;
+                }
+                return css`
+                    border-color: ${palette.dark_cyan};
+                `;
             }
-        `}
+        }}
+        &:disabled {
+            background-image: url('/static/svg/common/selector/disabled_register_selector_down_arrow.svg');
+            background-color: ${palette.gray_f7};
+            border-color: ${palette.gray_e5};
+            color: ${palette.gray_e5};
+            cursor: not-allowed;
+        }
+    }
 `;
 
 interface IProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+    label?: string;
     options?: string[];
     value?: string;
     disabledOptions?: string[];
-    isValid: boolean;
+    isValid?: boolean;
+    useValidation?: boolean;
+    errorMessage?: string;
+    type?: 'register' | 'normal';
 }
 
 const Selector: React.FC<IProps> = ({
+    label,
     options = [],
     disabledOptions = [],
     isValid,
+    useValidation = true,
+    errorMessage = '옵션을 선택하세요.',
+    type = 'normal',
     ...props
 }) => {
     const validateMode = useSelector((state) => state.common.validateMode);
     return (
-        <Container isValid={!!isValid} validateMode={validateMode}>
-            <select {...props}>
-                {disabledOptions.map((option, index) => (
-                    <option key={index} value={option} disabled>
-                        {option}
-                    </option>
-                ))}
-                {options.map((option, index) => (
-                    <option key={index} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
+        <Container isValid={!!isValid} validateMode={validateMode} type={type}>
+            <label>
+                {label && <span>{label}</span>}
+                <select {...props}>
+                    {disabledOptions.map((option, index) => (
+                        <option key={index} value={option} disabled>
+                            {option}
+                        </option>
+                    ))}
+                    {options.map((option, index) => (
+                        <option key={index} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            {useValidation && validateMode && !isValid && (
+                <div className="selector-warning">
+                    <WarningIcon />
+                    <p>{errorMessage}</p>
+                </div>
+            )}
         </Container>
     );
 };
