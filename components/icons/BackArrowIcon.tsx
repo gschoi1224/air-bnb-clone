7134e742aaecdb9d8991248c9dbdd27116b535ef
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-01-27 21:54:50 +0900

    숙소 등록하기 공통 푸터 적용

---
+++ components/icons/BackArrowIcon.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import BackArrowIcon from '../../public/static/svg/register/register_room_footer_back_arrow.svg';
+
+export default React.memo(BackArrowIcon);
--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -7,6 +7,7 @@ import { registerRoomActions } from '../../store/registerRoom';
 import palette from '../../styles/palette';
 import RadioGroup from '../common/RadioGroup';
 import Selector from '../common/Selector';
+import RegisterRoomFooter from './RegisterRoomFooter';
 
 const Container = styled.div`
     padding: 62px 30px 100px;
@@ -162,6 +163,11 @@ const RegisterRoomBuilding: React.FC = () => {
                     />
                 </div>
             )}
+            <RegisterRoomFooter
+                isValid={false}
+                prevHref="/"
+                nextHref="/room/register/bedrooms"
+            />
         </Container>
     );
 };
