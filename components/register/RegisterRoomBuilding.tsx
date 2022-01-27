1f9cc9eaa6c3491424eed604733c643e77b400f6
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-01-27 17:04:13 +0900

    숙소 등록하기 1단계 컴포넌트 생성

--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -5,6 +5,7 @@ import { buildingTypeList, largeBuildingTypeList } from '../../lib/staticData';
 import { useSelector } from '../../store';
 import { registerRoomActions } from '../../store/registerRoom';
 import palette from '../../styles/palette';
+import RadioGroup from '../common/RadioGroup';
 import Selector from '../common/Selector';
 
 const Container = styled.div`
@@ -23,9 +24,46 @@ const Container = styled.div`
         width: 320px;
         margin-bottom: 32px;
     }
+    .register-room-room-type-radio {
+        max-width: 485px;
+        margin-bottom: 50px;
+    }
+    .register-room-is-setup-for-guest-radio {
+        margin-bottom: 50px;
+    }
 `;
 
 const disabledLargeBuildingTypeOptions = ['하나를 선택해주세요.'];
+const roomTypeRadioOptions = [
+    {
+        label: '집 전체',
+        value: 'entire',
+        description:
+            '게스트가 숙소 전체를 다른 사람과 공유하지 않고 단독으로 이용합니다. 일반적으로 침실, 욕실, 부엌이 포함됩니다.',
+    },
+    {
+        label: '개인실',
+        value: 'private',
+        description:
+            '게스트에게 개인 침실이 제공됩니다. 침실 이외의 공간은 공용일 수 있습니다.',
+    },
+    {
+        label: '다인실',
+        value: 'public',
+        description:
+            '게스트는 개인 공간 없이, 다른 사람과 함께 쓰는 침실이나 공용공간에서 숙박합니다.',
+    },
+];
+const isSetUpForGuestOptions = [
+    {
+        label: '예, 게스트용으로 따로 마련된 숙소입니다.',
+        value: true,
+    },
+    {
+        label: '아니요, 제 개인 물건이 숙소에 있습니다.',
+        value: false,
+    },
+];
 
 const RegisterRoomBuilding: React.FC = () => {
     const largeBuildingType = useSelector(
@@ -34,6 +72,10 @@ const RegisterRoomBuilding: React.FC = () => {
     const buildingType = useSelector(
         (state) => state.registerRoom.buildingType
     );
+    const isSetUpForGuest = useSelector(
+        (state) => state.registerRoom.isSetUpForGuest
+    );
+    const roomType = useSelector((state) => state.registerRoom.roomType);
     const dispatch = useDispatch();
 
     // 큰 범위 건물 유형 변경 시
@@ -46,13 +88,33 @@ const RegisterRoomBuilding: React.FC = () => {
     // 선택된 건물 유형 options
     const detailBuildingOptions = useMemo(() => {
         const houseBuildingTypeList = buildingTypeList[largeBuildingType];
-        dispatch(registerRoomActions.setBuildingType(houseBuildingTypeList[0]));
+        if (houseBuildingTypeList && houseBuildingTypeList.length > 0) {
+            dispatch(
+                registerRoomActions.setBuildingType(
+                    houseBuildingTypeList || houseBuildingTypeList[0]
+                )
+            );
+        }
         return houseBuildingTypeList;
     }, [largeBuildingType]);
 
     // 상세 건물 유형 변경 시
     const onChangeBuildingType = (e: React.ChangeEvent<HTMLSelectElement>) => {
         dispatch(registerRoomActions.setBuildingType(e.target.value));
+    };
+
+    // 숙소 유형 변경시
+    const onChangeRoomType = (value: 'entire' | 'private' | 'public') => {
+        dispatch(
+            registerRoomActions.setRoomType(
+                value as 'entire' | 'private' | 'public'
+            )
+        );
+    };
+
+    // 게스트용 숙소인지 변경시
+    const onChangeIsSetUpForGuest = (value: any) => {
+        dispatch(registerRoomActions.setIsSetUpForGuest(value));
     };
 
     return (
@@ -80,6 +142,26 @@ const RegisterRoomBuilding: React.FC = () => {
                     onChange={onChangeBuildingType}
                 />
             </div>
+            {buildingType && (
+                <div className="register-room-room-type-radio">
+                    <RadioGroup
+                        label="게스트가 묵게 될 숙소 유형을 골라주세요."
+                        value={roomType}
+                        options={roomTypeRadioOptions}
+                        onChange={onChangeRoomType}
+                    />
+                </div>
+            )}
+            {roomType && (
+                <div className="register-room-is-setup-for-guest-radio">
+                    <RadioGroup
+                        label="게스트만 사용하도록 만들어진 숙소인가요?"
+                        value={isSetUpForGuest}
+                        onChange={onChangeIsSetUpForGuest}
+                        options={isSetUpForGuestOptions}
+                    />
+                </div>
+            )}
         </Container>
     );
 };
