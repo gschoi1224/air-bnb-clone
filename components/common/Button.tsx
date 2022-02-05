b6eb4abc5fa89e267bcfa10767926adae22ec950
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-02-06 01:32:12 +0900

    사이즈 조정 기능 추가

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -39,9 +39,26 @@ const getButtonColor = (color: string, colorReverse: boolean) => {
     }
 };
 
+const getButtonSize = (size: 'small' | 'medium') => {
+    switch (size) {
+        case 'medium':
+            return css`
+                height: 48px;
+            `;
+        case 'small':
+            return css`
+                font-size: 14px;
+                height: 36px;
+            `;
+        default:
+            return '';
+    }
+};
+
 interface StyledButtonProps {
     width: string | undefined;
     colorReverse: boolean;
+    size: 'small' | 'medium';
 }
 
 const Container = styled.button<StyledButtonProps>`
@@ -59,6 +76,7 @@ const Container = styled.button<StyledButtonProps>`
     cursor: pointer;
     width: ${(props) => props.width};
     ${(props) => getButtonColor(props.color || '', props.colorReverse)};
+    ${(props) => getButtonSize(props.size)}
     svg {
         margin-right: 12px;
     }
@@ -70,6 +88,7 @@ interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     width?: string;
     colorReverse?: boolean;
     icon?: JSX.Element;
+    size?: 'small' | 'medium';
 }
 
 const Button: React.FC<IProps> = ({
@@ -78,6 +97,7 @@ const Button: React.FC<IProps> = ({
     width,
     colorReverse = false,
     icon,
+    size = 'medium',
     ...props
 }) => {
     return (
@@ -86,6 +106,7 @@ const Button: React.FC<IProps> = ({
             color={color}
             width={width}
             colorReverse={colorReverse}
+            size={size}
         >
             {icon}
             {children}
