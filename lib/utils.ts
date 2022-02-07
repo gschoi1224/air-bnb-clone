9818f2320a75e9779124f959a03b5d986df81f42
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2022-02-08 00:33:01 +0900

    queryString 함수 제작

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ export const cookieStringToObject = (cookieString: string | undefined) => {
     const cookies: { [key: string]: string } = {};
     if (cookieString) {
         const itemString = cookieString?.split(/\s*;\s*/);
-        itemString.forEach((pairs) => {
+        itemString.forEach(pairs => {
             const pair = pairs.split(/\s*=\s*/);
             cookies[pair[0]] = pair.splice(1).join('=');
         });
@@ -22,3 +22,23 @@ export const getNumber = (string: string) => {
 
 // ,가 포함된 금액 형식으로 입력값 변경하는 함수
 export const makeMoneyString = (money: number) => money.toLocaleString();
+
+// query string 만들기
+export const makeQueryString = (
+    baseUrl: string,
+    queriesObject: Object & { [key: string]: any }
+) => {
+    const keys = Object.keys(queriesObject);
+    const values = Object.values(queriesObject);
+    if (keys.length === 0) {
+        return baseUrl;
+    }
+    let queryString = `${baseUrl}?`;
+    keys.forEach((key, i) => {
+        if (queriesObject[key]) {
+            queryString += `${keys[i]}=${values[i]}&`;
+        }
+    });
+    // 마지막 & 제거
+    return queryString.slice(0, -1);
+};
